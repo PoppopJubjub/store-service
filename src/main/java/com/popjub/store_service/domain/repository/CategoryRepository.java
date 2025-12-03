3c2f2b4ec64381d24c25584671bf50e6f3696762
raebagi <prh0208@naver.com>
2025-12-03 15:50:58 +0900

    refactor : Repository 분리 DIP 원칙 적용을 위한 Repository 인터페이스 구현/분리

--- src/main/java/com/popjub/store_service/domain/repository/CategoryRepository.java
+++ src/main/java/com/popjub/store_service/domain/repository/CategoryRepository.java
@@ -1,11 +1,19 @@
 package com.popjub.store_service.domain.repository;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.popjub.store_service.domain.entity.Category;
 
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+//JPA 말고 다른 ORM이 들어 왔을 때 서비스 코드가 변경되지 않기 위해 Repository분리
+//의존성 역전
+public interface CategoryRepository{
+	//나 이런 이름의 메소드를 사용할거야
 	boolean existsByName(String name);
+	List<Category> findAllById(List<Long> categoryIds);
+	Category save(Category category);
 }
+
---
+++ src/main/java/com/popjub/store_service/infrastructure/repository/CategoryJpaRepository.java
@@ -0,0 +1,10 @@
+package com.popjub.store_service.infrastructure.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.popjub.store_service.domain.entity.Category;
+
+public interface CategoryJpaRepository extends JpaRepository<Category,Long> {
+
+	boolean existsByName(String name);
+}
