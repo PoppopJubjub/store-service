6624877f0187341a15668bee5e0b62e375140746
raebagi <prh0208@naver.com>
2025-12-03 12:36:50 +0900

    refactor : @Transaction(readonly=true) 추가

--- src/main/java/com/popjub/store_service/application/service/CategoryService.java
+++ src/main/java/com/popjub/store_service/application/service/CategoryService.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CategoryService {
 
 	private final CategoryRepository categoryRepository;
