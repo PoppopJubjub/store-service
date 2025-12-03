3615fe6b2541d3e22a2330a8d99afe4848cb9373
raebagi <prh0208@naver.com>
2025-12-03 15:58:29 +0900

    refactor: Command/Result 패턴 도입 및 DTO 구조 리팩토링 * Presentation → Application 계층 분리를 위해 Command/Result 적용 * Commad/Result 적용에 의한 DTO 구조 리팩토링(toEntity, from 추가)

--- src/main/java/com/popjub/store_service/presentation/dto/request/CreateCategoryRequest.java
+++ src/main/java/com/popjub/store_service/presentation/dto/request/CreateCategoryRequest.java
@@ -1,5 +1,7 @@
 package com.popjub.store_service.presentation.dto.request;
 
+import com.popjub.store_service.application.dto.command.CreateCategoryCommand;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -7,4 +9,8 @@ public record CreateCategoryRequest(
 	@NotBlank(message = "카테고리 이름은 필수입니다.")
 	@Size(max = 50, message = "카테고리 이름은 최대 50자까지 가능합니다.")
 	String categoryName
-) {	}
+) {
+	public CreateCategoryCommand toCommand(){
+		return new CreateCategoryCommand(categoryName);
+	}
+}
--- src/main/java/com/popjub/store_service/presentation/dto/response/CreateCategoryResponse.java
+++ src/main/java/com/popjub/store_service/presentation/dto/response/CreateCategoryResponse.java
@@ -1,11 +1,14 @@
 package com.popjub.store_service.presentation.dto.response;
 
+import com.popjub.store_service.application.dto.result.CreateCategoryResult;
 import com.popjub.store_service.domain.entity.Category;
 
 public record CreateCategoryResponse(
 	Long categoryId
 ) {
-	public static CreateCategoryResponse from(Category category){
-		return new CreateCategoryResponse(category.getCategoryId());
+	public static CreateCategoryResponse from(CreateCategoryResult result) {
+		return new CreateCategoryResponse(
+			result.categoryId()
+		);
 	}
 }
