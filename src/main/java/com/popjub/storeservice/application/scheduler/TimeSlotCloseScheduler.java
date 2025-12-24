66d304e507604b4050a3009a3f74a4a072c6baeb
raebagi <prh0208@naver.com>
2025-12-24 22:14:58 +0900

    refactor : 중복되는 @Transactional 제거

--- src/main/java/com/popjub/storeservice/application/scheduler/TimeSlotCloseScheduler.java
+++ src/main/java/com/popjub/storeservice/application/scheduler/TimeSlotCloseScheduler.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.popjub.storeservice.application.service.TimeSlotService;
 
@@ -18,7 +17,6 @@ public class TimeSlotCloseScheduler {
 
 	private final TimeSlotService timeSlotService;
 
-	@Transactional
 	@Scheduled(cron = "0 */10 8-22 * * *" , zone = "Asia/Seoul")
 	/*@Scheduled(fixedDelay = 10_000, initialDelay = 1_000) //테스트용*/
 	public void closeTimeSlot() {
